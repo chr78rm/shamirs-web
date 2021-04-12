2bdf166388770b3b58a4753b8b27281163edeef1
Christof Reichardt <chr78rm@onlinehome.de>
2021-04-12 17:07:47 +0200

    Nested if-then-else replaced by guard clauses.

--- shamirs-service/src/main/java/de/christofreichardt/restapp/shamir/resource/SessionRS.java
+++ shamirs-service/src/main/java/de/christofreichardt/restapp/shamir/resource/SessionRS.java
@@ -139,51 +139,55 @@ public class SessionRS implements Traceable {
             tracer.wayout();
         }
     }
-    
+
     Response activateSession(String keystoreId, String sessionId, JsonObject sessionInstructions) {
         AbstractTracer tracer = getCurrentTracer();
         tracer.entry("Response", this, "activateSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
 
         try {
             Response response;
-            Optional<DatabasedKeystore> optionalKeystore = this.keystoreService.findByIdWithActiveSlicesAndCurrentSession(keystoreId);
-            if (optionalKeystore.isPresent()) {
-                DatabasedKeystore keystore = optionalKeystore.get();
-                Session currentSession = keystore.getSessions().iterator().next();
-                if (Objects.equals(currentSession.getId(), sessionId)) {
-                    try {
-                        ShamirsProtection shamirsProtection = new ShamirsProtection(keystore.sharePoints());
-                        currentSession.setPhase(Session.Phase.ACTIVE);
-                        JsonObject automaticClose = sessionInstructions.getJsonObject("activation").getJsonObject("automaticClose");
-                        int idleTime = automaticClose.getInt("idleTime");
-                        TemporalUnit temporalUnit = ChronoUnit.valueOf(automaticClose.getString("temporalUnit", "SECONDS"));
-                        Duration duration = Duration.of(idleTime, temporalUnit);
-                        currentSession.setIdleTime(duration.getSeconds());
-                        currentSession.setModificationTime(LocalDateTime.now());
-                        currentSession.setExpirationTime(currentSession.getModificationTime().plusSeconds(duration.getSeconds()));
-                        this.sessionService.save(currentSession);
-
-                        response = Response.status(Response.Status.OK)
-                                .entity(currentSession.toJson())
-                                .type(MediaType.APPLICATION_JSON)
-                                .encoding("UTF-8")
-                                .build();
-                    } catch (IllegalArgumentException ex) {
-                        String message = String.format("Cannot activate session for Keystore[id=%s].", keystoreId);
-                        tracer.logException(LogLevel.ERROR, ex, getClass(), "activateSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
-                        ErrorResponse errorResponse = new ErrorResponse(Response.Status.BAD_REQUEST, message, ex.getMessage());
-                        response = errorResponse.build();
-                    }
-                } else {
-                    String message = String.format("No current Session[id=%s] found for Keystore[id=%s].", sessionId, keystoreId);
-                    tracer.logMessage(LogLevel.ERROR, message, getClass(), "activateSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
-                    ErrorResponse errorResponse = new ErrorResponse(Response.Status.BAD_REQUEST, message);
-                    response = errorResponse.build();
-                }
-            } else {
+            Optional<DatabasedKeystore> keystore = this.keystoreService.findByIdWithActiveSlicesAndCurrentSession(keystoreId);
+            if (keystore.isEmpty()) {
                 String message = String.format("No such Keystore[id=%s].", keystoreId);
-                tracer.logMessage(LogLevel.ERROR, message, getClass(), "activateSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
+                tracer.logMessage(LogLevel.ERROR, message, getClass(), "closeSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
                 ErrorResponse errorResponse = new ErrorResponse(Response.Status.BAD_REQUEST, message);
+                return errorResponse.build();
+            }
+            if (keystore.get().getSessions().isEmpty()) {
+                String message = String.format("Couldn't retrieve a session for Keystore[id=%s].", keystoreId);
+                tracer.logMessage(LogLevel.ERROR, message, getClass(), "closeSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
+                ErrorResponse errorResponse = new ErrorResponse(Response.Status.INTERNAL_SERVER_ERROR, message);
+                return errorResponse.build();
+            }
+            Session currentSession = keystore.get().getSessions().iterator().next();
+            if (!Objects.equals(currentSession.getId(), sessionId)) {
+                String message = String.format("No active Session[id=%s] found for Keystore[id=%s].", sessionId, keystoreId);
+                tracer.logMessage(LogLevel.ERROR, message, getClass(), "closeSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
+                ErrorResponse errorResponse = new ErrorResponse(Response.Status.BAD_REQUEST, message);
+                return errorResponse.build();
+            }
+            
+            try {
+                ShamirsProtection shamirsProtection = new ShamirsProtection(keystore.get().sharePoints());
+                currentSession.setPhase(Session.Phase.ACTIVE);
+                JsonObject automaticClose = sessionInstructions.getJsonObject("activation").getJsonObject("automaticClose");
+                int idleTime = automaticClose.getInt("idleTime");
+                TemporalUnit temporalUnit = ChronoUnit.valueOf(automaticClose.getString("temporalUnit", "SECONDS"));
+                Duration duration = Duration.of(idleTime, temporalUnit);
+                currentSession.setIdleTime(duration.getSeconds());
+                currentSession.setModificationTime(LocalDateTime.now());
+                currentSession.setExpirationTime(currentSession.getModificationTime().plusSeconds(duration.getSeconds()));
+                this.sessionService.save(currentSession);
+
+                response = Response.status(Response.Status.OK)
+                        .entity(currentSession.toJson())
+                        .type(MediaType.APPLICATION_JSON)
+                        .encoding("UTF-8")
+                        .build();
+            } catch (IllegalArgumentException ex) {
+                String message = String.format("Cannot activate session for Keystore[id=%s].", keystoreId);
+                tracer.logException(LogLevel.ERROR, ex, getClass(), "activateSession(String keystoreId, String sessionId, JsonObject sessionInstructions)");
+                ErrorResponse errorResponse = new ErrorResponse(Response.Status.BAD_REQUEST, message, ex.getMessage());
                 response = errorResponse.build();
             }
 
