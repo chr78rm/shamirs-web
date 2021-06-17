78e0955925cda96047dab8fbbcd43151cef12c44
Christof Reichardt <chr78rm@onlinehome.de>
2021-06-17 17:45:24 +0200

    Generating self-signed CA certificates done right.

--- pki/generate-intermediate-ca.sh
+++ pki/generate-intermediate-ca.sh
@@ -7,7 +7,7 @@ ROOT_PASSWORD=QgpzVx8eZ33iDGIwLi48dE4vT
 
 # generate key pair
 keytool -genkeypair -alias intermediate-ca -keyalg RSA -keysize 2048 -sigalg SHA256withRSA -dname "cn=cr-intermediate, L=Rodgau, ST=Hessen, c=DE" \
--keypass ${IM_PASSWORD} -validity 1825 -storetype pkcs12 -keystore ${IM_KEYSTORE_FILE} -storepass ${IM_PASSWORD} -v
+-keypass ${IM_PASSWORD} -validity 1825 -storetype pkcs12 -keystore ${IM_KEYSTORE_FILE} -storepass ${IM_PASSWORD} -v 
 
 # request pkcs10 certification
 keytool -certreq -alias intermediate-ca -sigalg SHA256withRSA -file certreq.p10 -keypass ${IM_PASSWORD} -keystore ${IM_KEYSTORE_FILE} \
@@ -15,7 +15,7 @@ keytool -certreq -alias intermediate-ca -sigalg SHA256withRSA -file certreq.p10 
 
 # process pkcs10 certification request for intermediate-ca
 keytool -gencert -rfc -infile certreq.p10 -outfile intermediate-ca.pem -alias root-ca -sigalg SHA256withECDSA -validity 365 \
--keypass ${ROOT_PASSWORD} -storepass ${ROOT_PASSWORD} -storetype pkcs12 -keystore ${ROOT_KEYSTORE_FILE}
+-keypass ${ROOT_PASSWORD} -storepass ${ROOT_PASSWORD} -storetype pkcs12 -keystore ${ROOT_KEYSTORE_FILE} -ext BasicConstraints=ca:true,PathLen:2
 
 # import root certificate
 keytool -importcert -noprompt -alias root-ca -file root-ca.pem -keystore ${IM_KEYSTORE_FILE} -storepass ${IM_PASSWORD} -v
