233d93120bc9b5f82d5e958f10217f611feb689f
Christof Reichardt <chr78rm@onlinehome.de>
2021-07-19 19:13:59 +0200

    Added another host name required for the Docker bridge.

--- sql/mariadb/create-database.sql
+++ sql/mariadb/create-database.sql
@@ -1,4 +1,6 @@
 CREATE OR REPLACE USER 'shamir'@'localhost' IDENTIFIED BY 'Msiw47Ut129';
+CREATE OR REPLACE USER 'shamir'@'172.17.0.1' IDENTIFIED BY 'Msiw47Ut129';
 CREATE OR REPLACE DATABASE shamirs_db;
 GRANT ALL ON shamirs_db.* TO 'shamir'@'localhost';
+GRANT ALL ON shamirs_db.* TO 'shamir'@'172.17.0.1';
 GRANT FILE ON *.* TO 'shamir'@'localhost';
