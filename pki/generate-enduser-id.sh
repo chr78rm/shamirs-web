32ceb1ea919af7ac01053d5379d97c8c97d4c364
Christof Reichardt <chr78rm@onlinehome.de>
2022-02-14 16:42:32 +0100

    PKI scripts improved.

--- pki/generate-enduser-id.sh
+++ pki/generate-enduser-id.sh
@@ -8,6 +8,7 @@ ALIAS_REGEX="^--alias=[a-z0-9-]{1,25}$"
 ENDUSER_KEYSTORE_REGEX="^--keystore=[A-Za-z]+[A-Za-z0-9-]{1,50}$"
 CERTIFICATE_VALIDITY_REGEX="^--cert-validity=[0-9]{2,4}$"
 COMMON_NAME_REGEX="^--common-name=[A-Za-z0-9-]{1,50}$"
+IM_PW_REGEX="^--password=[A-Za-z0-9]{1,25}$"
 
 # defaults
 COMMON_NAME_ARG=localhost
@@ -30,6 +31,10 @@ do
 	then
 		COMMON_NAME_ARG=${ARG:14}
 	fi
+	if [[ ${ARG} =~ ${IM_PW_REGEX} ]]
+	then
+		IM_PASSWORD=${ARG:11}
+	fi
 done
 echo alias=${ALIAS_ARG}
 echo keystore=${KEYSTORE_ARG}
@@ -38,40 +43,39 @@ echo common-name=${COMMON_NAME_ARG}
 
 # constants
 IM_KEYSTORE_FILE=my-intermediate-ca.p12
-IM_PASSWORD=Rqu4u0rmgbp0fsO1LHSAoV2Be
 ENDUSER_KEYSTORE_FILE=${KEYSTORE_ARG}.p12
 ENDUSER_PASSWORD=changeit
 ENDUSER_TRUSTSTORE_FILE=${KEYSTORE_ARG}-trust.p12
 DNAME="CN=${COMMON_NAME_ARG}, L=Rodgau, ST=Hessen, C=DE"
 
 # generate key pair
-keytool -genkeypair -alias ${ALIAS_ARG} -keyalg RSA -keysize 2048 -sigalg SHA256withRSA -dname "${DNAME}" \
+keytool -J-Duser.language=en -genkeypair -alias ${ALIAS_ARG} -keyalg RSA -keysize 2048 -sigalg SHA256withRSA -dname "${DNAME}" \
 -keypass ${ENDUSER_PASSWORD} -validity 1825 -storetype pkcs12 -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
 
 # request pkcs10 certification
-keytool -certreq -alias ${ALIAS_ARG} -sigalg SHA256withRSA -file certreq.p10 -keypass ${ENDUSER_PASSWORD} -keystore ${ENDUSER_KEYSTORE_FILE} \
+keytool -J-Duser.language=en -certreq -alias ${ALIAS_ARG} -sigalg SHA256withRSA -file certreq.p10 -keypass ${ENDUSER_PASSWORD} -keystore ${ENDUSER_KEYSTORE_FILE} \
 -storepass ${ENDUSER_PASSWORD} -storetype pkcs12
 
 # process pkcs10 certification request for enduser id
-keytool -gencert -rfc -infile certreq.p10 -outfile ${KEYSTORE_ARG}.pem -alias intermediate-ca -sigalg SHA256withRSA -validity ${CERTIFICATION_VALIDITY_ARG} -startdate +0d \
+keytool -J-Duser.language=en -gencert -rfc -infile certreq.p10 -outfile ${KEYSTORE_ARG}.pem -alias intermediate-ca -sigalg SHA256withRSA -validity ${CERTIFICATION_VALIDITY_ARG} -startdate +0d \
 -keypass ${IM_PASSWORD} -storepass ${IM_PASSWORD} -storetype pkcs12 -keystore ${IM_KEYSTORE_FILE}
 
 # import root certificate
-keytool -importcert -noprompt -alias root-ca -file root-ca.pem -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -importcert -noprompt -alias root-ca -file root-ca.pem -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
 
 # import intermediate certificate
-keytool -importcert -noprompt -alias intermediate-ca -file intermediate-ca.pem -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -importcert -noprompt -alias intermediate-ca -file intermediate-ca.pem -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
 
 # import the certificate reply for enduser-id
-keytool -importcert -keystore ${ENDUSER_KEYSTORE_FILE} -alias ${ALIAS_ARG} -file ${KEYSTORE_ARG}.pem -keypass ${ENDUSER_PASSWORD} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -importcert -keystore ${ENDUSER_KEYSTORE_FILE} -alias ${ALIAS_ARG} -file ${KEYSTORE_ARG}.pem -keypass ${ENDUSER_PASSWORD} -storepass ${ENDUSER_PASSWORD} -v
 
 # list entries
-keytool -list -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -list -keystore ${ENDUSER_KEYSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
 
 # make truststore with certificates
-keytool -importcert -noprompt -alias root-ca -file root-ca.pem -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
-keytool -importcert -alias intermediate-ca -file intermediate-ca.pem -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
-keytool -importcert -alias ${ALIAS_ARG} -file ${KEYSTORE_ARG}.pem -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -importcert -noprompt -alias root-ca -file root-ca.pem -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -importcert -alias intermediate-ca -file intermediate-ca.pem -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -importcert -alias ${ALIAS_ARG} -file ${KEYSTORE_ARG}.pem -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
 
 # list entries
-keytool -list -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
+keytool -J-Duser.language=en -list -keystore ${ENDUSER_TRUSTSTORE_FILE} -storepass ${ENDUSER_PASSWORD} -v
--- pki/generate-root-ca.sh
+++ pki/generate-root-ca.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+set -o errexit # terminate on error
+ARGS=$* # all parameter
+
+# defaults
 ROOT_KEYSTORE_FILE=my-root-ca.p12
-ROOT_PASSWORD=QgpzVx8eZ33iDGIwLi48dE4vT
+
+# evaluate parameter
+ROOT_PW_REGEX="^--password=[A-Za-z0-9]{1,25}$"
+for ARG in ${ARGS} 
+do
+	if [[ ${ARG} =~ ${ROOT_PW_REGEX} ]]
+	then
+		ROOT_PASSWORD=${ARG:11}
+	fi
+done
 
 # generate key pair
-keytool -genkeypair -alias root-ca -keyalg EC -groupname secp521r1 -sigalg SHA256withECDSA -dname "cn=cr-root, L=Rodgau, ST=Hessen, c=DE" \
+keytool -J-Duser.language=en -genkeypair -alias root-ca -keyalg EC -groupname secp521r1 -sigalg SHA256withECDSA -dname "cn=cr-root, L=Rodgau, ST=Hessen, c=DE" \
 -keypass ${ROOT_PASSWORD} -validity 1825 -storetype pkcs12 -keystore ${ROOT_KEYSTORE_FILE} -storepass ${ROOT_PASSWORD} -v \
 -ext BasicConstraints=ca:true,PathLen:3
 
 # export certificate
-keytool -exportcert -rfc -alias root-ca -file root-ca.pem -keystore ${ROOT_KEYSTORE_FILE} -storepass ${ROOT_PASSWORD} -v
+keytool -J-Duser.language=en -exportcert -rfc -alias root-ca -file root-ca.pem -keystore ${ROOT_KEYSTORE_FILE} -storepass ${ROOT_PASSWORD} -v
